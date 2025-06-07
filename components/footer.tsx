10bb06c5f8892ce71ac5ba5979dee6084bd57d8e
Simon Synnes <simonsynnes@gmail.com>
2025-06-07 14:21:29 +0200

    minor text update

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2024 Simon Synnes. All rights reserved.
+        &copy; 2025 Simon Synnes. All rights reserved.
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
